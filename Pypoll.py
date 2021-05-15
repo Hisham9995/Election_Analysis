b5f27d7dc48dd7b7cdee522194c6bce0021f73b3
HISHAM ALMANSURI <82621077+Hisham9995@users.noreply.github.com>
2021-05-15 19:06:51 -0400

    Pypoll.py

---
+++ Pypoll.py
@@ -0,0 +1,96 @@
+# The data we need to retrieve.
+# 1. The total number of votes
+# 2. A complete list of candidates who received votes
+# 3. The percentage of votes each candidate won
+# 4. The total number of votes each candidate won
+# 5. The winner of the election based on popular vote.
+
+#Import the datetime .
+import datetime as dt
+#Use the now() attribute on the datetime class to get the present time
+now = dt.datetime.now()
+#Print 
+print("The time right now is", now)
+
+#dependencies
+import csv
+import os
+file_to_load = os.path.join("Resources","election_results.csv")
+
+# Assign a variable to save the file to a path.
+election_data = open(file_to_load, 'r')
+
+# To do: perform analysis.
+
+# Close the file.
+election_data.close()
+
+# Open the election results and read the file
+import csv
+file_to_load = os.path.join("Resources", "election_results.csv")
+with open(file_to_load) as election_data:
+
+# To do: perform analysis.
+ print(election_data)
+
+ 
+ #Indirect Path to the File
+
+import csv
+# Assign a variable for the file to load and the path.
+file_to_load = os.path.join("Resources","election_results.csv")
+# Open the election results and read the file.
+with open (file_to_load) as election_data:
+#print
+ print("election_data")
+
+ # Create a filename variable to a direct or indirect path to the file.
+file_to_save = os.path.join("analysis","election_analysis.txt")
+# Using the open() function with the "w" mode we will write data to the file.
+open(file_to_save, "w")
+
+# Create a filename variable to a direct or indirect path to the file.
+file_to_save = os.path.join("analysis","election_analysis.text")
+
+# Using the with statement open the file as a text file.
+outfile = open (file_to_save, "w")
+
+# Write some data to the file
+outfile.write("Hello world")
+
+# Close the file
+outfile.close
+
+# Create a filename variable to a direct or indirect path to the file.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# Using the with statement open the file as a text file.
+with open(file_to_save, "w") as txt_file:
+ 
+ 
+ # Write three counties to the file.
+ txt_file.write ("\nCounties in the Election")
+ txt_file.write ("\n-------------------------")
+ txt_file.write ("\nArapahoe, Denver, Jefferson")
+
+# Or  txt_file.write("Arapahoe, ") /  txt_file.write("Denver, ") /txt_file.write("Jefferson, ")
+ txt_file.write("\nArapahoe,\n Denver,\n Jefferson")
+
+# Add our dependencies.
+
+
+# Add our dependencies.
+import csv
+import os
+# Assign a variable to load a file from a path.
+file_to_load = os.path.join("Resources", "election_results.csv")
+# Assign a variable to save the file to a path.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# Open the election results and read the file.
+with open(file_to_load) as election_data:
+    file_reader = csv.reader(election_data)
+
+# Read and print the header row.
+    headers = next(file_reader)
+    print(headers)
